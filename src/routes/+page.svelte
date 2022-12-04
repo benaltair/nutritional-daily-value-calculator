b0b54d36e5be249fc79190a165d3fd69866e3ffb
Ben Altair <ben@benaltair.com>
2022-12-04 15:54:52 -0800

    Install simple CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import 'simpledotcss/simple.min.css';
+</script>
+
+<header>
+	<h1>Daily Value Calculator</h1>
+	<p>Converting the percentage of daily value from food labels in Canada</p>
+</header>
+
