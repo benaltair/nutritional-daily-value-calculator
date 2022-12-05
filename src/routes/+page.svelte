94eac78c6bfcbb9cf6a9d0d94deca06d78e8f98d
Ben Altair <ben@benaltair.com>
2022-12-04 16:15:10 -0800

    Create table with calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import 'simpledotcss/simple.min.css';
+	import { dailyValues } from '$lib/dailyValues';
+	let dailyValuesTable = dailyValues;
 </script>
 
 <header>
@@ -7,3 +9,42 @@
 	<p>Converting the percentage of daily value from food labels in Canada</p>
 </header>
 
+<table>
+	<thead>
+		<tr>
+			<th>Nutrient</th>
+			<th>Percentage</th>
+			<th>Serving Amount</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each dailyValuesTable as { name, key, unit, value }, i}
+			<tr>
+				<td>{name}</td>
+				<td>
+					<input
+						type="number"
+						bind:value={dailyValuesTable[i].percentage}
+						name="percentage-{key}"
+						id="{key}-input"
+					/>%
+				</td>
+				<td>
+					{#if dailyValuesTable[i].percentage}
+						{Math.round(value * dailyValuesTable[i].percentage) / 100} {unit}
+					{/if}
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		min-width: 100%;
+	}
+	input {
+		width: calc(100% - 2ch);
+		min-width: 4ch;
+	}
+</style>
